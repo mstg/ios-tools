294a592c25613fd1e49668ba5340d41e935dfe99
Mustafa Gezen <mustiigezen@gmail.com>
2016-02-16 09:52:27 +0100

    Create printioservices.c

---
+++ printioservices.c
@@ -0,0 +1,37 @@
+// Prints all IOServices
+
+int main(int argc, char * argv[]) {
+  kern_return_t err;
+  io_iterator_t i;
+  err = IOServiceGetMatchingServices(kIOMasterPortDefault, IOServiceMatching("IOService"), &i);
+  
+  if (err != KERN_SUCCESS) {
+    fprintf(stderr,"IOServiceGetMatchingServices: error\n");
+    exit(1);
+  }
+	
+  io_service_t temp;
+  kern_return_t kr;
+  io_name_t serviceName;
+  io_name_t service_before;
+  int count = 0;
+  
+  printf("Print all services\n----------------\n");
+  while(IOIteratorIsValid(i) && (temp = IOIteratorNext(i))) {
+    kr = IORegistryEntryGetName(temp, serviceName);
+    if (strcmp(service_before, serviceName) == 0) continue;
+    strcpy(service_before, serviceName);
+    if (kr != KERN_SUCCESS) {
+      IOObjectRelease(temp);
+      printf("Can't get name of service!\n");
+      continue;
+    }
+		
+    printf("%s\n", serviceName);
+    count++;
+  }
+	
+  printf("Count: %d\n", count);
+	
+  printf("\n");
+}
